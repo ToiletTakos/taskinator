e78b4cfed9a8ecc28535ed7122302d2c169bfc02
ToiletTakos <dan.pandolfo@gmail.com>
2021-09-09 10:54:56 -0400

    properly saved data on local

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,8 @@ var completeEditTask = function(taskName,taskType, taskId){
         }
     }
 
+    saveTasks();
+
     alert("Task Updated!");
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
@@ -82,6 +84,8 @@ var taskStatusChangeHandler = function(event){
             tasks[i].status = statusValue;
         }
     }
+
+    saveTasks();
 }
 
 var createTaskEl = function(taskDataObj) {
@@ -109,6 +113,8 @@ var createTaskEl = function(taskDataObj) {
     taskDataObj.id = taskIdCounter;
 
     tasks.push(taskDataObj);
+
+    saveTasks();
 
     taskIdCounter++;
 }
@@ -200,10 +206,16 @@ var deleteTask = function(taskId) {
     }
 
     //reassign tasks array to be the same as updatedTaskArr
-    tasks = updatedTasksArr;
+    tasks = updatedTaskArr;
+
+    saveTasks();
 }
 
 var tasks = [] ;
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
